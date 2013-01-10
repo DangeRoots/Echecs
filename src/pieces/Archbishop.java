df6f140f4931690baf7504344886c5482cdb8c77
DangeRoots <icham.s@live.fr>
2013-01-10 11:38:36 +0100

    Ajout princesse et imperatrice (variante capablanca)

---
+++ src/pieces/Archbishop.java
@@ -0,0 +1,41 @@
+package pieces;
+
+import game.Plate;
+
+import java.util.ArrayList;
+
+import javax.swing.Icon;
+
+public class Archbishop extends Piece {
+
+	@Override
+	public ArrayList<Piece> accessibleCells(Plate plate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	boolean canMove(Piece piece) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Icon getIcon() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	boolean move(Piece piece) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/pieces/Chancellor.java
@@ -0,0 +1,41 @@
+package pieces;
+
+import game.Plate;
+
+import java.util.ArrayList;
+
+import javax.swing.Icon;
+
+public class Chancellor extends Piece {
+
+	@Override
+	public ArrayList<Piece> accessibleCells(Plate plate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	boolean canMove(Piece piece) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Icon getIcon() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	boolean move(Piece piece) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+
+	}
+
+}

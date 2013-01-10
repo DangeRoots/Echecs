df0b86a0d640496cb9c379371c3c83e827a8f572
DangeRoots <icham.s@live.fr>
2013-01-10 10:54:54 +0100

    Deplacement Queen OK

--- src/pieces/Queen.java
+++ src/pieces/Queen.java
@@ -17,13 +17,18 @@ public class Queen extends Piece {
 			return null;
 		}
 		else{
+
 			Rook rook = new Rook();
+			rook.setColor(this.getColor());
+			rook.setPosition(this.getColumn(),this.getRow());
 			ArrayList<Piece> accessRook = new ArrayList<Piece>();
-			accessRook=rook.accessibleCells(plate);
-			for (int i = 0; i < accessRook.size();i++)
+			accessRook = rook.accessibleCells(plate);
+			for (int i=0;i<accessRook.size();i++)
 				accessibles.add(accessRook.get(i));
 			Bishop bishop = new Bishop();
 			ArrayList<Piece> accessBishop = new ArrayList<Piece>();
+			bishop.setColor(this.getColor());
+			bishop.setPosition(this.getColumn(), this.getRow());
 			accessBishop=bishop.accessibleCells(plate);
 			for (int i = 0; i < accessBishop.size();i++)
 				accessibles.add(accessBishop.get(i));

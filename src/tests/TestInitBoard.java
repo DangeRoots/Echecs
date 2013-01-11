a6eb68b9937733f81f60f755ac24c5d2fd2f0c9a
DangeRoots <icham.s@live.fr>
2013-01-11 08:36:18 +0100

    Ajout tests construction plateau (classique & capablanca)

---
+++ src/tests/TestInitBoard.java
@@ -0,0 +1,20 @@
+package tests;
+
+import game.Plate;
+import junit.framework.TestCase;
+
+
+public class TestInitBoard extends TestCase {
+	
+	public void testInitBoardClassic() {
+		
+		Plate g_plateau = new Plate();		
+		assertEquals(8, g_plateau.getBoardSize());
+	}
+
+	public void testInitBoardCapablanca() {
+		
+		Plate g_plateau = new Plate("capablanca");		
+		assertEquals(10, g_plateau.getBoardSize());
+	}
+}
